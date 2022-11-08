70d7c8fee0c58c811db6c11258a12171c3eb0cf3
Alka Lama <82252528+alkawaiba@users.noreply.github.com>
2022-11-08 13:54:49 +0545

    Update Esewa.jsx

--- frontend/src/components/Payment/Esewa/Esewa.jsx
+++ frontend/src/components/Payment/Esewa/Esewa.jsx
@@ -3,37 +3,124 @@ import './Esewa.css'
 import {v4 as uuidv4} from 'uuid'
 var path="https://uat.esewa.com.np/epay/main";
 
-const Esewa = () => {
- 
-  var params= {
-    amt : 100,
+const Esewa = ({ price, membership }) => {
+  var params = {
+    amt: price,
     psc: 0,
     pdc: 0,
     txAmt: 0,
-    tAmt: 100,
+    tAmt: price,
     pid: uuidv4(),
     scd: "EPAYTEST",
     su: "http://localhost:3000/success",
+    // su: "http://merchant.com.np/page/esewa_payment_success",
     // fu: "http://merchant.com.np/page/esewa_payment_failed"
-    fu : "http://localhost:3000/failure"
-}
+    fu: "http://localhost:3000/failure",
+  };
 
-  return (
-    
-    <form className = "esewaForm" action={path} method = "POST">
-    {  Object.entries(params).map(([key, value]) => {
-        return <input type = "hidden" value = {value} name = {key}/>
+  var params1 = {
+    amt: price,
+    psc: 0,
+    pdc: 0,
+    txAmt: 0,
+    tAmt: price,
+    pid: uuidv4(),
+    scd: "EPAYTEST",
+    su: "http://localhost:3000/success-member",
+    // su: "http://merchant.com.np/page/esewa_payment_success",
+    // fu: "http://merchant.com.np/page/esewa_payment_failed"
+    fu: "http://localhost:3000/failure",
+  };
+
+  return membership ? (
+    <form className="esewaForm" action={path} method="POST">
+      {Object.entries(params1).map(([key, value]) => {
+        return <input type="hidden" value={value} name={key} />;
       })}
-       <button type = "submit" style = {{cursor : "pointer", border : "none", outline : "none"}}>
-                        <img style = {{width : "130px", objectFit : "cover", height : "40px"}} src="https://i2.wp.com/insidergeeks.com/wp-content/uploads/2020/09/eSewa-Digital-Wallet-Nepal.jpeg?resize=696%2C464&ssl=1
-                    " alt="" />                    
-                        
-                    </button>
-
-
+      <button className='snbtn'
+        type="submit"
+        style={{ cursor: "pointer", border: "none", outline: "none" }}
+      >
+        START NOW
+      </button>
     </form>
-   
- )
-}
+  ) : (
+    <form className="esewaForm" action={path} method="POST">
+      {Object.entries(params).map(([key, value]) => {
+        return <input type="hidden" value={value} name={key} />;
+      })}
+      <button className='cobtn'
+        type="submit"
+        style={{ cursor: "pointer", border: "none", outline: "none" }}
+      >
+        CHECK OUT
+      </button>
+    </form>
+  );
+};
 
-export default Esewa
+export default Esewa;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Esewa = ({price,membership,setMember}) => {
+ 
+//   var params= {
+//     amt : price,
+//     psc: 0,
+//     pdc: 0,
+//     txAmt: 0,
+//     tAmt: price,
+//     pid: uuidv4(),
+//     scd: "EPAYTEST",
+//     su: "http://localhost:3000/success",
+//     // fu: "http://merchant.com.np/page/esewa_payment_failed"
+//     fu : "http://localhost:3000/failure"
+// }
+
+//   // const clickhandler = () => {
+//   // setMember(true)
+// // }
+//   return ( 
+
+//       <form className = "esewaForm" action={path} method = "POST">
+
+//     {  Object.entries(params).map(([key, value]) => {
+//         return <input type = "hidden" value = {value} name = {key}/>
+//       })} 
+
+//       {membership?<button className = "cartSummaryButton" type="submit" >START NOW</button>:
+//       <button className = "cartSummaryButton" type="submit">CHECKOUT NOW</button>}
+//       </form>
+        
+
+      
+//        /* <button type = "submit" style = {{cursor : "pointer", border : "none", outline : "none"}}>
+//                         <img style = {{width : "130px", objectFit : "cover", height : "40px"}} src="https://i2.wp.com/insidergeeks.com/wp-content/uploads/2020/09/eSewa-Digital-Wallet-Nepal.jpeg?resize=696%2C464&ssl=1
+//                     " alt="" />                    
+                        
+//                     </button> */
+
+
+   
+//  )
+// }
+
+// export default Esewa
